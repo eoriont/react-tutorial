4009e35d43a5b482c82b4170be360ef35e62fda8
oriont <elijahotarr@gmail.com>
2019-01-20 18:07:58 -0800

    adding code for tut 1

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <p>Our React App!</p>
+        )
+    }
+}
+
+export default App;
